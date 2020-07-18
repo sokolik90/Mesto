d0a245a96cb727d3483b8cfc615e879ab8fd1ac5
Sviatoslav Sokolov <sokolik90@yandex.ru>
2020-07-18 20:44:21 +0300

    Delele options for babel-loader from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+            {
+            loader: "babel-loader",
+            }
+        ]
+      },
+      {
         test: /\.css$/i,
         use: [ 
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,{
